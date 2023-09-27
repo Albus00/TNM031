c85ef19ff234e63d1c6e977c033dd37ff65976ad
Albin Kjellberg <albin.kjellberg2000@gmail.com>
2023-09-27 15:59:44 +0200

    Kinda done

--- Lab 2 - Cryptology/src/Main.java
+++ Lab 2 - Cryptology/src/Main.java
@@ -56,5 +56,84 @@ public class Main {
         String message = gen.decryptMsg(msgToTranslate);
         System.out.println("Recovered message: " + message);
 
+        /*
+      import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+
+
+public class Main {
+    private static KeyPairGenerator keyGen;
+    private static PrivateKey priv;
+    public static PublicKey pub;
+
+    public static void main(String[] args)
+            throws IOException
+    {
+        // Enter data using BufferReader
+        BufferedReader reader = new BufferedReader(
+                new InputStreamReader(System.in));
+
+        //System.out.println("Choose your secret prime numbers");
+        //System.out.print("p: ");
+        //BigInteger p = new BigInteger(reader.readLine());
+        BigInteger p = new BigInteger("885320963");
+
+        //System.out.print("q: ");
+        //BigInteger q = new BigInteger(reader.readLine());
+        BigInteger q = new BigInteger("238855417");
+
+        keyGen = new KeyPairGenerator(p, q);
+        priv = keyGen.getPrivate();
+        pub = keyGen.getPublic();
+
+        System.out.println("Private: " + priv.getKey());
+        System.out.println("Public: " + pub.getKey());
+        System.out.println("Bob sends his public key to YOU");
+        System.out.println("...");
+
+        System.out.print("Input message: ");
+        String msg = reader.readLine();
+
+        MessageGenerator gen = new MessageGenerator();
+        String genMsg = gen.generateMsg(msg);
+        System.out.println("Message: " + genMsg);
+        String[] genMsgSplit = genMsg.split(" ");
+
+        // Encrypt the message
+        String c = "";
+        for (String s: genMsgSplit) {
+            System.out.println(s);
+            BigInteger msgSegment = new BigInteger(s);
+            BigInteger msgSegmentEncrypt = msgSegment.modPow(pub.e, pub.n);
+            System.out.println(msgSegmentEncrypt);
+            c += msgSegmentEncrypt.toString() + " ";
+        }
+
+        System.out.println("You send c = " + c + " to Bob");
+        System.out.println("...");
+
+        // Decrypt the message
+        System.out.println("Bob decrypts the message");
+        BigInteger d = pub.e.modInverse(keyGen.phiN);
+        System.out.println("d = " + d);
+
+        String[] genMsgSplit2 = c.split(" ");
+        String msgToTranslate = "";
+        for (String s: genMsgSplit2) {
+            System.out.println(s);
+            BigInteger msgSegment = new BigInteger(s);
+            BigInteger msgSegmentDecrypt = msgSegment.modPow(d, pub.n);
+            msgToTranslate += msgSegmentDecrypt.toString();
+            System.out.println(msgSegmentDecrypt.toString());
+        }
+        System.out.println("Message before translation: " + msgToTranslate);
+        String message = gen.decryptMsg(msgToTranslate);
+        System.out.println("Recovered message: " + message);
+
+    }
+}
+        * */
     }
 }
--- Lab 2 - Cryptology/src/MessageGenerator.java
+++ Lab 2 - Cryptology/src/MessageGenerator.java
@@ -3,12 +3,23 @@ public class MessageGenerator {
     public MessageGenerator() {}
 
     public String generateMsg(String msg) {
+        int splitCounter = 0;
         String genMsg = String.valueOf(alphabet.indexOf(msg.charAt(0)) + 1); // Start with the first char in msg, because this
         for (int i = 1; i < msg.length(); i++) {
             String letter = String.valueOf(alphabet.indexOf(msg.charAt(i)) + 1); // +1 to accommodate for a=01
             if (letter.length() < 2)
                 letter = "0" + letter;
             genMsg += letter;
+
+            /*
+            if (splitCounter == 8) {
+                genMsg += " ";
+                splitCounter = 0;
+            }
+            else {
+                splitCounter++;
+            }
+            */
         }
 
         return genMsg;
