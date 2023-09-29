34fbba37204b6ef101ea168841f637612d8022a6
Albin Kjellberg <albin.kjellberg2000@gmail.com>
2023-09-29 16:29:22 +0200

    Program runs

---
+++ Lab 3 - Secure Sockets/src/Client.java
@@ -0,0 +1,204 @@
+import java.io.*;
+import java.net.*;
+import javax.net.ssl.*;
+import java.security.*;
+
+public class Client {
+
+    private InetAddress host;
+    private int port;
+    // This is not a reserved port number
+    static final int DEFAULT_PORT = 8189;
+    static final String KEYSTORE = "LIUkeystore.ks";
+    static final String TRUSTSTORE = "LIUtruststore.ks";
+    static final String STOREPASSWD1 = "123456";
+    static final String STOREPASSWD2 = "abcdef";
+    static final String ALIASPASSWD = "123456";
+
+
+    public Client( InetAddress host, int port ) {
+        this.host = host;
+        this.port = port;
+    }
+    public void run() {
+        try {
+            KeyStore ks = KeyStore.getInstance( "JCEKS" );
+            ks.load(new FileInputStream(KEYSTORE), STOREPASSWD1.toCharArray());
+
+            KeyStore ts = KeyStore.getInstance( "JCEKS" );
+            ts.load(new FileInputStream(TRUSTSTORE), STOREPASSWD2.toCharArray());
+
+            KeyManagerFactory kmf = KeyManagerFactory.getInstance("SunX509");
+            kmf.init( ks, ALIASPASSWD.toCharArray() );
+
+            TrustManagerFactory tmf = TrustManagerFactory.getInstance( "SunX509" );
+            tmf.init(ts);
+
+            SSLContext sslContext = SSLContext.getInstance( "TLS" );
+            sslContext.init(kmf.getKeyManagers(), tmf.getTrustManagers(), null);
+            SSLSocketFactory sslFact = sslContext.getSocketFactory();
+            SSLSocket client =  (SSLSocket)sslFact.createSocket(host, port);
+            client.setEnabledCipherSuites( client.getSupportedCipherSuites() );
+            System.out.println("\n>>>> SSL/TLS handshake completed");
+
+            BufferedReader socketFromServer;
+            socketFromServer = new BufferedReader( new InputStreamReader( client.getInputStream() ) );
+            PrintWriter socketToServer = new PrintWriter( client.getOutputStream(), true );
+
+            printMenu();
+            String input = new BufferedReader(new InputStreamReader(System.in)).readLine();
+            int option = Integer.parseInt(input);
+
+            socketToServer.println(option);
+
+
+            switch(option) {
+                case 1:
+                    System.out.println("Enter the file name: ");
+                    try
+                    {
+                        String fileName = new BufferedReader(new InputStreamReader(System.in)).readLine();
+                        System.out.println("Downloading the file from the server");
+                        socketToServer.println(fileName);
+                        String fileData = dataFromServer(socketFromServer);
+                        //createTextFile(fileName, fileData);
+                    }
+                    catch (Exception e)
+                    {
+                        System.out.println("There was an error when handling your request");
+                    }
+                    break;
+                case 2:
+                    System.out.println("Please the name of the file that you want to upload:");
+                    try {
+                        String fileName = new BufferedReader(new InputStreamReader(System.in)).readLine();
+                        String fileData= readFile(fileName);
+                        System.out.println("Uploading file to the server");
+                        socketToServer.println(fileName);
+                        socketToServer.println(fileData);
+                    }
+                    catch (Exception e) {
+                        System.out.println("There was an error when handling your request");
+                        e.printStackTrace();
+                    }
+                    break;
+                case 3:
+                    System.out.println("Please enter the name of the file you want to delete.");
+                    try {
+                        String fileName = new BufferedReader(new InputStreamReader(System.in)).readLine();
+                        System.out.println("Deleting file from the server");
+                        socketToServer.println(fileName);
+                    }
+                    catch (Exception e){
+                        System.out.println("There was an error when handling your request");
+                        e.printStackTrace();
+                    }
+                    break;
+                default:
+                    System.out.println("Wrong option, byebye");
+                    break;
+            }
+
+
+
+        }
+        catch( Exception x ) {
+            System.out.println( x );
+            x.printStackTrace();
+        }
+    }
+
+    public void download() {
+
+    }
+
+    private String dataFromServer(BufferedReader socketFromServer)
+    {
+        try {
+            StringBuilder sb = new StringBuilder();
+            String line = socketFromServer.readLine();
+            String data="";
+            while (line!= null) {
+                sb.append(line);
+                sb.append(System.lineSeparator());
+                line = socketFromServer.readLine();
+            }
+            data = sb.toString();
+            return data;
+        }
+        catch (Exception e)
+        {
+            System.out.println ("Error reading from server: " + e.toString());
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    private void createFile(String fileName, String data)
+    {
+        String name = fileName;
+        System.out.println(name);
+        PrintWriter writer;
+        try {
+            writer = new PrintWriter(name);
+            writer.print(data);
+            writer.close();
+        }
+        catch (Exception e)
+        {
+            System.out.println("Error writing to file: " + e.toString());
+            e.printStackTrace();
+        }
+
+    }
+
+    private String readFile(String fileName)
+    {
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(fileName));
+            StringBuilder sb = new StringBuilder();
+            String line = br.readLine();
+
+            while(line!=null) {
+                sb.append(line);
+                sb.append(System.lineSeparator());
+                line = br.readLine();
+            }
+
+            return sb.toString();
+        }
+        catch (Exception e)
+        {
+            System.out.println("There was an error handling your request." + e.toString());
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public void printMenu() {
+        System.out.println("SSL Lab 3 Menu, have fun!");
+        System.out.println("1. Download file from server?");
+        System.out.println("2. Upload file to server?");
+        System.out.println("3. Delete file from server?");
+
+    }
+
+    public static void main( String[] args ) {
+        try {
+            InetAddress host = InetAddress.getLocalHost();
+            int port = DEFAULT_PORT;
+            if ( args.length > 0 ) {
+                port = Integer.parseInt( args[0] );
+            }
+            if ( args.length > 1 ) {
+                host = InetAddress.getByName( args[1] );
+            }
+            Client client = new Client( host, port );
+            client.run();
+        }
+        catch ( UnknownHostException uhx ) {
+            System.out.println( uhx );
+            uhx.printStackTrace();
+        }
+    }
+}
---
+++ Lab 3 - Secure Sockets/src/Server.java
@@ -0,0 +1,169 @@
+/**
+ * Created by englund on 26/09/15.
+ */
+
+import java.io.*;
+import javax.net.ssl.*;
+import java.security.*;
+
+public class Server {
+
+    private int port;
+    static final int DEFAULT_PORT = 8189;
+    static final String KEYSTORE = "LIUkeystore.ks";
+    static final String TRUSTSTORE = "LIUtruststore.ks";
+    static final String STOREPASSWD1 = "123456";
+    static final String STOREPASSWD2 = "abcdef";
+    static final String ALIASPASSWD = "123456";
+
+    Server( int port ) {
+        this.port = port;
+    }
+
+    public void run() {
+        try {
+            KeyStore ks = KeyStore.getInstance( "JCEKS" );
+            ks.load( new FileInputStream( KEYSTORE ), STOREPASSWD1.toCharArray() );
+
+            KeyStore ts = KeyStore.getInstance( "JCEKS" );
+            ts.load( new FileInputStream( TRUSTSTORE ), STOREPASSWD2.toCharArray() );
+
+            KeyManagerFactory kmf = KeyManagerFactory.getInstance( "SunX509" );
+            kmf.init( ks, ALIASPASSWD.toCharArray() );
+
+            TrustManagerFactory tmf = TrustManagerFactory.getInstance( "SunX509" );
+            tmf.init(ts);
+
+            SSLContext sslContext = SSLContext.getInstance( "TLS" );
+            sslContext.init(kmf.getKeyManagers(), tmf.getTrustManagers(), null);
+            SSLServerSocketFactory sslServerFactory = sslContext.getServerSocketFactory();
+            SSLServerSocket sss = (SSLServerSocket) sslServerFactory.createServerSocket( port );
+            sss.setEnabledCipherSuites( sss.getSupportedCipherSuites() );
+
+            System.out.println("\n>>>> SecureAdditionServer: active ");
+            SSLSocket incoming = (SSLSocket)sss.accept();
+
+            BufferedReader in = new BufferedReader( new InputStreamReader( incoming.getInputStream() ) );
+            PrintWriter out = new PrintWriter( incoming.getOutputStream(), true );
+
+            String fileName;
+            String fileData;
+            int option = Integer.parseInt(in.readLine());
+
+            switch(option) {
+                case 1:
+                    System.out.println("User requested to download a file");
+
+                    fileName = in.readLine();
+                    fileData = readFile(fileName);
+                    out.println(fileData);
+                    break;
+                case 2:
+                    System.out.println("User requested to upload a file");
+                    fileName = in.readLine();
+                    fileData = readStringFromClient(in);
+
+                    download(fileName, fileData);
+                    break;
+                case 3:
+                    System.out.println("User requested to delete a file");
+                    fileName = in.readLine();
+                    delete(fileName);
+                    break;
+                default:
+                    System.out.println("Unexpected behaviour");
+                    break;
+            }
+
+            incoming.close();
+
+        } catch(Exception x) {
+            System.out.println(x);
+            x.printStackTrace();
+        }
+    }
+
+    public void download(String fileName, String fileData) {
+        String serverFileName = "server"+fileName;
+        System.out.println(serverFileName);
+        PrintWriter writer;
+
+        try
+        {
+            writer = new PrintWriter(serverFileName, "UTF-8");
+            writer.print(fileData);
+            writer.close();
+        }
+        catch (Exception e)
+        {
+            System.out.println("Error writing to file");
+            e.printStackTrace();
+        }
+
+    }
+
+    private String readFile(String fileName)
+    {
+        try
+        {
+
+            BufferedReader br = new BufferedReader(new FileReader(fileName));
+            StringBuilder sb = new StringBuilder();
+            String line = br.readLine();
+
+            while(line!=null)
+            {
+                sb.append(line);
+                sb.append(System.lineSeparator());
+                line = br.readLine();
+            }
+            // return full text
+            return sb.toString();
+        }
+        catch (Exception e)
+        {
+            System.out.println("Couldn't read file " + fileName + " Exception: " + e.toString());
+            e.printStackTrace();
+            return "";
+        }
+    }
+    public void delete(String name) {
+        try {
+            File mFile = new File(name);
+            mFile.delete();
+            System.out.println("File deleted");
+        }
+        catch (Exception e){
+            System.out.println("Error when trying to delete file");
+            e.printStackTrace();
+        }
+    }
+
+    private String readStringFromClient(BufferedReader socketIn) {
+        try {
+            StringBuilder sb = new StringBuilder();
+            String line = socketIn.readLine();
+            String fullText = "";
+            while (line != null) {
+                sb.append(line);
+                sb.append(System.lineSeparator());
+                line = socketIn.readLine();
+            }
+            fullText = sb.toString();
+            return fullText;
+        } catch (Exception e) {
+            System.out.println("Error reading string from server");
+            e.printStackTrace();
+            return "";
+        }
+    }
+
+    public static void main(String[] args) {
+        int port = DEFAULT_PORT;
+        if (args.length > 0 ) {
+            port = Integer.parseInt( args[0] );
+        }
+        Server server = new Server( port );
+        server.run();
+    }
+}
